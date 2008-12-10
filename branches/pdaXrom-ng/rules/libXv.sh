560a03b9155025e3889b23f21362a8564a7ec950
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2008-12-10 22:15:07 +0000

    libXrandr libXtst libXv libXres

---
+++ branches/pdaXrom-ng/rules/libXv.sh
@@ -0,0 +1,48 @@
+#
+# packet template
+#
+# Copyright (C) 2008 by Alexander Chukov <sash@pdaXrom.org>
+#          
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+LIBXV=libXv-1.0.3.tar.bz2
+LIBXV_MIRROR=ftp://ftp.freedesktop.org/pub/xorg/X11R7.3/src/lib
+LIBXV_DIR=$BUILD_DIR/libXv-1.0.3
+LIBXV_ENV=
+
+build_libXv() {
+    test -e "$STATE_DIR/libXv-1.0.3" && return
+    banner "Build $LIBXV"
+    download $LIBXV_MIRROR $LIBXV
+    extract $LIBXV
+    apply_patches $LIBXV_DIR $LIBXV
+    pushd $TOP_DIR
+    cd $LIBXV_DIR
+    (
+    eval \
+	$CROSS_CONF_ENV \
+	$LIBXV_ENV \
+	./configure --host=$TARGET_ARCH \
+	    --prefix=/usr \
+	    --sysconfdir=/etc \
+	    --disable-malloc0returnsnull \
+	    || error
+    )
+    make $MAKEARGS || error
+
+    install_sysroot_files || error
+    
+    $INSTALL -D -m 644 src/.libs/libXv.so.1.0.0 $ROOTFS_DIR/usr/lib/libXv.so.1.0.0 || error
+    ln -sf libXv.so.1.0.0 $ROOTFS_DIR/usr/lib/libXv.so.1
+    ln -sf libXv.so.1.0.0 $ROOTFS_DIR/usr/lib/libXv.so
+    $STRIP $ROOTFS_DIR/usr/lib/libXv.so.1.0.0
+
+    popd
+    touch "$STATE_DIR/libXv-1.0.3"
+}
+
+build_libXv
---
+++ branches/pdaXrom-ng/rules/videoproto.sh
@@ -0,0 +1,42 @@
+#
+# packet template
+#
+# Copyright (C) 2008 by Alexander Chukov <sash@pdaXrom.org>
+#          
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+VIDEOPROTO=videoproto-2.2.2.tar.bz2
+VIDEOPROTO_MIRROR=ftp://ftp.freedesktop.org/pub/xorg/X11R7.3/src/proto
+VIDEOPROTO_DIR=$BUILD_DIR/videoproto-2.2.2
+VIDEOPROTO_ENV=
+
+build_videoproto() {
+    test -e "$STATE_DIR/videoproto-2.2.2" && return
+    banner "Build $VIDEOPROTO"
+    download $VIDEOPROTO_MIRROR $VIDEOPROTO
+    extract $VIDEOPROTO
+    apply_patches $VIDEOPROTO_DIR $VIDEOPROTO
+    pushd $TOP_DIR
+    cd $VIDEOPROTO_DIR
+    (
+    eval \
+	$CROSS_CONF_ENV \
+	$VIDEOPROTO_ENV \
+	./configure --host=$TARGET_ARCH \
+	    --prefix=/usr \
+	    --sysconfdir=/etc \
+	    || error
+    )
+    make $MAKEARGS || error
+
+    install_sysroot_files || error
+
+    popd
+    touch "$STATE_DIR/videoproto-2.2.2"
+}
+
+build_videoproto
