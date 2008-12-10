d2f379ae5e7e1667dc42a528be13e41bbc1f3c3c
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2008-12-10 19:38:24 +0000

    libXft

---
+++ branches/pdaXrom-ng/rules/libXft.sh
@@ -0,0 +1,48 @@
+#
+# packet template
+#
+# Copyright (C) 2008 by Alexander Chukov <sash@pdaXrom.org>
+#          
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+LIBXFT=libXft-2.1.12.tar.bz2
+LIBXFT_MIRROR=ftp://ftp.freedesktop.org/pub/xorg/X11R7.3/src/lib
+LIBXFT_DIR=$BUILD_DIR/libXft-2.1.12
+LIBXFT_ENV=
+
+build_libXft() {
+    test -e "$STATE_DIR/libXft-2.1.12" && return
+    banner "Build $LIBXFT"
+    download $LIBXFT_MIRROR $LIBXFT
+    extract $LIBXFT
+    apply_patches $LIBXFT_DIR $LIBXFT
+    pushd $TOP_DIR
+    cd $LIBXFT_DIR
+    (
+    eval \
+	$CROSS_CONF_ENV \
+	$LIBXFT_ENV \
+	./configure --host=$TARGET_ARCH \
+	    --prefix=/usr \
+	    --sysconfdir=/etc || error
+    )
+    make $MAKEARGS || error
+
+    install_sysroot_files || error
+    
+    $INSTALL -D -m 644 src/.libs/libXft.so.2.1.2 $ROOTFS_DIR/usr/lib/libXft.so.2.1.2 || error
+    ln -sf libXft.so.2.1.2 $ROOTFS_DIR/usr/lib/libXft.so.2
+    ln -sf libXft.so.2.1.2 $ROOTFS_DIR/usr/lib/libXft.so
+    $STRIP $ROOTFS_DIR/usr/lib/libXft.so.2.1.2
+
+    ln -sf $TARGET_BIN_DIR/bin/xft-config $HOST_BIN_DIR/bin/
+
+    popd
+    touch "$STATE_DIR/libXft-2.1.12"
+}
+
+build_libXft
