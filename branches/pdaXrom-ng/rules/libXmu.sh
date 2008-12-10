953a01b71d5ba30eb21f133d2d2305c86fed2134
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2008-12-10 20:51:50 +0000

    libXmu

---
+++ branches/pdaXrom-ng/rules/libXmu.sh
@@ -0,0 +1,51 @@
+#
+# packet template
+#
+# Copyright (C) 2008 by Alexander Chukov <sash@pdaXrom.org>
+#          
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+LIBXMU=libXmu-1.0.3.tar.bz2
+LIBXMU_MIRROR=ftp://ftp.freedesktop.org/pub/xorg/X11R7.3/src/lib
+LIBXMU_DIR=$BUILD_DIR/libXmu-1.0.3
+LIBXMU_ENV=
+
+build_libXmu() {
+    test -e "$STATE_DIR/libXmu-1.0.3" && return
+    banner "Build $LIBXMU"
+    download $LIBXMU_MIRROR $LIBXMU
+    extract $LIBXMU
+    apply_patches $LIBXMU_DIR $LIBXMU
+    pushd $TOP_DIR
+    cd $LIBXMU_DIR
+    (
+    eval \
+	$CROSS_CONF_ENV \
+	$LIBXMU_ENV \
+	./configure --host=$TARGET_ARCH \
+	    --prefix=/usr \
+	    --sysconfdir=/etc || error
+    )
+    make $MAKEARGS || error
+
+    install_sysroot_files || error
+    
+    $INSTALL -D -m 644 src/.libs/libXmu.so.6.2.0 $ROOTFS_DIR/usr/lib/libXmu.so.6.2.0 || error
+    ln -sf libXmu.so.6.2.0 $ROOTFS_DIR/usr/lib/libXmu.so.6
+    ln -sf libXmu.so.6.2.0 $ROOTFS_DIR/usr/lib/libXmu.so
+    $STRIP $ROOTFS_DIR/usr/lib/libXmu.so.6.2.0
+
+    $INSTALL -D -m 644 src/.libs/libXmuu.so.1.0.0 $ROOTFS_DIR/usr/lib/libXmuu.so.1.0.0 || error
+    ln -sf libXmuu.so.1.0.0 $ROOTFS_DIR/usr/lib/libXmuu.so.1
+    ln -sf libXmuu.so.1.0.0 $ROOTFS_DIR/usr/lib/libXmuu.so
+    $STRIP $ROOTFS_DIR/usr/lib/libXmuu.so.1.0.0
+
+    popd
+    touch "$STATE_DIR/libXmu-1.0.3"
+}
+
+build_libXmu
