34e6921579069e77ee7e81e16ba42519c3869d39
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2008-12-28 22:00:50 +0000

    lxrandr, lxterminal, lxnm

---
+++ branches/pdaXrom-ng/rules/libvte.sh
@@ -0,0 +1,51 @@
+#
+# packet template
+#
+# Copyright (C) 2008 by Alexander Chukov <sash@pdaXrom.org>
+#          
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+LIBVTE=vte-0.19.4.tar.bz2
+LIBVTE_MIRROR=http://ftp.gnome.org/pub/GNOME/sources/vte/0.19
+LIBVTE_DIR=$BUILD_DIR/vte-0.19.4
+LIBVTE_ENV="$CROSS_ENV_AC"
+
+build_libvte() {
+    test -e "$STATE_DIR/libvte.installed" && return
+    banner "Build libvte"
+    download $LIBVTE_MIRROR $LIBVTE
+    extract $LIBVTE
+    apply_patches $LIBVTE_DIR $LIBVTE
+    pushd $TOP_DIR
+    cd $LIBVTE_DIR
+    (
+    eval \
+	$CROSS_CONF_ENV \
+	$LIBVTE_ENV \
+	./configure --build=$BUILD_ARCH --host=$TARGET_ARCH \
+	    --prefix=/usr \
+	    --sysconfdir=/etc \
+	    --disable-glade \
+	    --disable-gtk-doc \
+	    --disable-python \
+	    || error
+    ) || error "configure"
+    
+    make $MAKEARGS || error
+
+    install_sysroot_files || error
+    
+    $INSTALL -D -m 644 src/.libs/libvte.so.9.4.3 $ROOTFS_DIR/usr/lib/libvte.so.9.4.3 || error
+    ln -sf libvte.so.9.4.3 $ROOTFS_DIR/usr/lib/libvte.so.9
+    ln -sf libvte.so.9.4.3 $ROOTFS_DIR/usr/lib/libvte.so
+    $STRIP $ROOTFS_DIR/usr/lib/libvte.so.9.4.3
+
+    popd
+    touch "$STATE_DIR/libvte.installed"
+}
+
+build_libvte
