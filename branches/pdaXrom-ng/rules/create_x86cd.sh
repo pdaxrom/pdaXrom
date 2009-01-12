ec66552cc6a10481dd67272d24a0d5bebf002ab6
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-01-12 16:28:55 +0000

    x86 cd quiet boot

--- branches/pdaXrom-ng/rules/create_x86cd.sh
+++ branches/pdaXrom-ng/rules/create_x86cd.sh
@@ -8,11 +8,11 @@ create_x86cd() {
     cp -f $IMAGES_DIR/bzImage    $T/boot/
     cp -f $HOST_BIN_DIR/share/syslinux/isolinux.bin $T/isolinux/
     printf "DEFAULT /boot/bzImage
-APPEND  initrd=/boot/rootfs.img root=/dev/ram0 vga=0x314
+APPEND  initrd=/boot/rootfs.img root=/dev/ram0 vga=0x314 quiet
 LABEL live
   menu label ^Try pdaXrom in vesa mode
   kernel /boot/bzImage
-  append initrd=/boot/rootfs.img root=/dev/ram0 vga=0x314
+  append initrd=/boot/rootfs.img root=/dev/ram0 vga=0x314 quiet
 LABEL safe
   menu label ^Try pdaXrom in text mode
   kernel /boot/bzImage
