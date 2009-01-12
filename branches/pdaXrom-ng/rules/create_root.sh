e610ed1a4a963fbfa2e8b0ada741c253ce19978b
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-01-12 16:06:55 +0000

    shutdown script

--- branches/pdaXrom-ng/rules/create_root.sh
+++ branches/pdaXrom-ng/rules/create_root.sh
@@ -38,9 +38,10 @@ create_root() {
 	mkdir -p "$ROOTFS_DIR/$d"
     done
     
-    $INSTALL -m 755 $GENERICFS_DIR/etc/init.d/rcS $ROOTFS_DIR/etc/init.d/
-    $INSTALL -m 644 $GENERICFS_DIR/etc/init.d/functions $ROOTFS_DIR/etc/init.d/
-    $INSTALL -m 755 $GENERICFS_DIR/etc/init.d/sysklogd $ROOTFS_DIR/etc/init.d/
+    $INSTALL -m 755 $GENERICFS_DIR/etc/init.d/rcS $ROOTFS_DIR/etc/init.d/ || error
+    $INSTALL -m 755 $GENERICFS_DIR/etc/init.d/rcK $ROOTFS_DIR/etc/init.d/ || error
+    $INSTALL -m 644 $GENERICFS_DIR/etc/init.d/functions $ROOTFS_DIR/etc/init.d/ || error
+    $INSTALL -m 755 $GENERICFS_DIR/etc/init.d/sysklogd $ROOTFS_DIR/etc/init.d/ || error
     install_rc_start sysklogd 05
 
     for f in $GENERICFS_DIR/etc/*; do
