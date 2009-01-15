b9800ddb5bab632dddbe1277340648fe53f9f29f
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-01-15 14:48:53 +0000

    ps3 uclibc basic image

---
+++ branches/pdaXrom-ng/build-linux-uclibc-ps3-basic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TARGET_ARCH="powerpc-linux-uclibc"
+TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
+TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
+
+KERNEL_CONFIG=ps3_kernel_config
+
+. ./packages-basic.inc
+
+. $RULES_DIR/ps3-utils.sh
+. $RULES_DIR/tweak-ps3.sh
+. $RULES_DIR/create_initramfs.sh
+. $RULES_DIR/create_ps3cd.sh
