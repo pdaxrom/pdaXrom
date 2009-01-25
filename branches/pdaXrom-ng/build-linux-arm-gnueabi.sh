24bfe7526980d95a430dc2cc6771c39dab4263f7
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-01-25 18:10:08 +0000

    linux-arm* images

---
+++ branches/pdaXrom-ng/build-linux-arm-gnueabi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+TARGET_ARCH="armel-linux-gnueabi"
+TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
+TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
+
+KERNEL_VERSION="2.6.28"
+KERNEL_CONFIG=versatile-kernel-2.6.28
+
+. ./packages-basic.inc
+. ./packages-mmlibs.inc
+. ./packages-libs.inc
+
+. ./packages-xorg-xlib.inc
+. ./packages-xorg-xserver.inc
+. ./packages-xorg-drivers.inc
+. ./packages-xorg-apps.inc
+. ./packages-xorg-fonts.inc
+
+#. ./packages-emulators.inc
+
+. ./packages-x-apps.inc
+
+. ./packages-hal.inc
+
+. ./packages-x-lxde.inc
+
+. ./packages-devel.inc
+
+. $RULES_DIR/create_initramfs.sh
