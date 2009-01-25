bc711636cc53610f318ebaa369d0d44b11197aa3
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-01-25 22:51:32 +0000

    bootstrap gcc to configure glibc headers

--- branches/pdaXrom-ng/rules/cross-linux-eglibc-tools.sh
+++ branches/pdaXrom-ng/rules/cross-linux-eglibc-tools.sh
@@ -156,12 +156,81 @@ GCC="gcc-4.3.2.tar.bz2"
 GCC_MIRROR="ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.3.2"
 GCC_DIR="$BUILD_DIR/gcc-4.3.2"
 
-build_gcc_stage1() {
-    test -e "$STATE_DIR/gcc_stage1" && return
-    banner "Build gcc $GCC stage 1"
+build_gcc_bootstrap() {
+    test -e "$STATE_DIR/gcc_bootstrap" && return
+    banner "Build gcc $GCC for configure glibc headers"
     download $GCC_MIRROR $GCC
     extract $GCC
     apply_patches $GCC_DIR $GCC
+    local CONF_ARGS=""
+    local CONF_CPU=""
+    case $TARGET_ARCH in
+    powerpc*-*|ppc*-*)
+	CONF_ARGS="--enable-secureplt \
+		    --enable-targets=powerpc-linux,powerpc64-linux \
+		    --with-cpu=default32 \
+		    --with-long-double-128"
+	;;
+    i*86-*)
+	if [ "x$DEFAULT_CPU" = "x" ]; then
+	    DEFAULT_CPU=${TARGET_ARCH/-*/}
+	fi
+	CONF_ARGS="--disable-cld \
+		    --with-arch=${DEFAULT_CPU} \
+		    --with-long-double-128"
+	;;
+    arm*)
+	CONF_ARGS=""
+	;;
+    *)
+	error "Unknown arch"
+	;;
+    esac
+    echo "configure"
+    pushd $TOP_DIR
+    mkdir "$GCC_DIR/build_bootstr"
+    cd $GCC_DIR/build_bootstr
+    ../configure --target=$TARGET_ARCH --prefix=$TOOLCHAIN_PREFIX \
+	--exec-prefix=$TOOLCHAIN_PREFIX --with-sysroot=$TOOLCHAIN_SYSROOT \
+	--libexecdir=$TOOLCHAIN_PREFIX/lib \
+	--without-headers \
+	--with-newlib \
+	--disable-decimal-float \
+	--disable-libffi \
+	--enable-linux-futex \
+	--disable-threads \
+	--disable-shared \
+	--enable-languages=c \
+	--enable-c99 \
+	--enable-long-long \
+	--enable-cmath \
+	--disable-libgomp \
+	--disable-libssp \
+	--disable-libmudflap \
+	--disable-multilib \
+	--disable-nls \
+	--disable-werror \
+	--with-gnu-as \
+	--with-gnu-ld \
+	--with-system-zlib \
+	--disable-libstdcxx-pch \
+	--enable-__cxa_atexit \
+	--with-gmp=$HOST_BIN_DIR \
+	--with-mpfr=$HOST_BIN_DIR \
+	$CONF_ARGS \
+	--disable-bootstrap || error "configure"
+    make $MAKEARGS MAKEINFO=true || error "make"
+    make MAKEINFO=true install || error "make install"
+    popd
+
+    ln -sf ${TARGET_ARCH}-gcc $TOOLCHAIN_PREFIX/bin/${TARGET_ARCH}-cc
+
+    touch "$STATE_DIR/gcc_bootstrap"
+}
+
+build_gcc_stage1() {
+    test -e "$STATE_DIR/gcc_stage1" && return
+    banner "Build gcc $GCC stage 1"
     local CONF_ARGS=""
     local CONF_CPU=""
     case $TARGET_ARCH in
@@ -300,11 +369,13 @@ build_gcc() {
     touch "$STATE_DIR/gcc"
 }
 
+build_binutils
+
 install_linux_headers
 
-install_glibc_headers
+build_gcc_bootstrap
 
-build_binutils
+install_glibc_headers
 
 build_gcc_stage1
 
