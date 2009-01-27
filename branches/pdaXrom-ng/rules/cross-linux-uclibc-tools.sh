cbe38095a2a4330523293f75cdf59de35287bca9
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-01-27 06:57:49 +0000

    default arch optimization for uclibc toolchain

--- branches/pdaXrom-ng/rules/cross-linux-uclibc-tools.sh
+++ branches/pdaXrom-ng/rules/cross-linux-uclibc-tools.sh
@@ -8,6 +8,12 @@ build_binutils() {
     download $BINUTILS_MIRROR $BINUTILS
     extract $BINUTILS
     apply_patches $BINUTILS_DIR $BINUTILS
+    local CONF_ARGS=
+    case $TARGET_ARCH in
+    powerpc*|ppc*)
+	CONF_ARGS="--enable-targets=spu"
+	;;
+    esac
     echo "configure"
     pushd $TOP_DIR
     mkdir "$BINUTILS_DIR/build"
@@ -15,7 +21,7 @@ build_binutils() {
     ../configure --target=$TARGET_ARCH --prefix=$TOOLCHAIN_PREFIX \
 	--exec-prefix=$TOOLCHAIN_PREFIX --with-sysroot=$TOOLCHAIN_SYSROOT \
 	--enable-werror=no --enable-64-bit-bfd --disable-debug \
-	--disable-shared || error "configure"
+	--disable-shared $CONF_ARGS || error "configure"
     make $MAKEARGS MAKEINFO=true || error "make"
     make $MAKEARGS MAKEINFO=true install || error "make install"
     popd
@@ -109,6 +115,39 @@ install_uclibc_headers() {
     touch "$STATE_DIR/uClibc-headers.installed"
 }
 
+build_uClibc() {
+    test -e "$STATE_DIR/uClibc.installed" && return
+    banner "Build uClibc"
+
+    pushd $TOP_DIR
+    cd $UCLIBC_DIR
+
+    make distclean
+
+    if [ "x$UCLIBC_CONFIG" = "x" ]; then
+	cp $CONFIG_DIR/uClibc/${TARGET_ARCH/-*/}-config .config || error "no uClibc config for ${TARGET_ARCH/-*/}"
+	case $TARGET_ARCH in
+	arm*-*eabi*)
+	    if grep -q "^CONFIG_AEABI=y" $KERNEL_DIR/.config ; then
+		sed -i -e "s:CONFIG_ARM_OABI.*:# CONFIG_ARM_OABI is not set:g" .config
+		sed -i -e "s:# CONFIG_ARM_EABI.*:CONFIG_ARM_EABI=y:g" .config
+	    fi
+	    ;;
+	esac
+    else
+	cp $CONFIG_DIR/uClibc/$UCLIBC_CONFIG .config || error "can't copy config, check config name in UCLIBC_CONFIG"
+    fi
+    
+    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} oldconfig || error
+    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} PREFIX=$TOOLCHAIN_SYSROOT DEVEL_PREFIX=/usr/ RUNTIME_PREFIX=/ install_headers || error
+
+    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} || error
+    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} PREFIX=$TOOLCHAIN_SYSROOT DEVEL_PREFIX=/usr/ RUNTIME_PREFIX=/ install || error
+
+    popd
+    touch "$STATE_DIR/uClibc.installed"
+}
+
 GCC="gcc-4.3.2.tar.bz2"
 GCC_MIRROR="ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.3.2"
 GCC_DIR="$BUILD_DIR/gcc-4.3.2"
@@ -124,13 +163,16 @@ build_gcc_stage1() {
     powerpc*-*|ppc*-*)
 	CONF_ARGS="--enable-secureplt \
 		    --enable-targets=powerpc-linux,powerpc64-linux \
-		    --with-cpu=default32"
+		    --with-cpu=${DEFAULT_CPU-default32} \
+		    --with-long-double-128"
 	;;
-    i*86-*|x86_64-*|amd64-*)
-	CONF_ARGS="--disable-cld"
+    i*86-*)
+	CONF_ARGS="--disable-cld \
+		    --with-arch=${DEFAULT_CPU-${TARGET_ARCH/-*/}} \
+		    --with-long-double-128"
 	;;
     arm*)
-	CONF_ARGS=""
+	CONF_ARGS="--with-arch=${DEFAULT_CPU-armv5te}"
 	;;
     *)
 	error "Unknown arch"
@@ -175,39 +217,6 @@ build_gcc_stage1() {
     touch "$STATE_DIR/gcc-stage1"
 }
 
-build_uClibc() {
-    test -e "$STATE_DIR/uClibc.installed" && return
-    banner "Build uClibc"
-
-    pushd $TOP_DIR
-    cd $UCLIBC_DIR
-
-    make distclean
-
-    if [ "x$UCLIBC_CONFIG" = "x" ]; then
-	cp $CONFIG_DIR/uClibc/${TARGET_ARCH/-*/}-config .config || error "no uClibc config for ${TARGET_ARCH/-*/}"
-	case $TARGET_ARCH in
-	arm*-*eabi*)
-	    if grep -q "^CONFIG_AEABI=y" $KERNEL_DIR/.config ; then
-		sed -i -e "s:CONFIG_ARM_OABI.*:# CONFIG_ARM_OABI is not set:g" .config
-		sed -i -e "s:# CONFIG_ARM_EABI.*:CONFIG_ARM_EABI=y:g" .config
-	    fi
-	    ;;
-	esac
-    else
-	cp $CONFIG_DIR/uClibc/$UCLIBC_CONFIG .config || error "can't copy config, check config name in UCLIBC_CONFIG"
-    fi
-    
-    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} oldconfig || error
-    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} PREFIX=$TOOLCHAIN_SYSROOT DEVEL_PREFIX=/usr/ RUNTIME_PREFIX=/ install_headers || error
-
-    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} || error
-    make $MAKEARGS KERNEL_HEADERS=$TOOLCHAIN_SYSROOT/usr/include CROSS=${CROSS} PREFIX=$TOOLCHAIN_SYSROOT DEVEL_PREFIX=/usr/ RUNTIME_PREFIX=/ install || error
-
-    popd
-    touch "$STATE_DIR/uClibc.installed"
-}
-
 build_gcc() {
     test -e "$STATE_DIR/gcc" && return
     banner "Build gcc $GCC"
@@ -216,13 +225,18 @@ build_gcc() {
     powerpc*-*|ppc*-*)
 	CONF_ARGS="--enable-secureplt \
 		    --enable-targets=powerpc-linux,powerpc64-linux \
-		    --with-cpu=default32"
+		    --with-cpu=${DEFAULT_CPU-default32} \
+		    --enable-libgomp \
+		    --with-long-double-128"
 	;;
-    i*86-*|x86_64-*|amd64-*)
-	CONF_ARGS="--disable-cld"
+    i*86-*)
+	CONF_ARGS="--disable-cld \
+		    --with-arch=${DEFAULT_CPU-${TARGET_ARCH/-*/}} \
+		    --with-long-double-128"
 	;;
     arm*)
-	CONF_ARGS=""
+	CONF_ARGS="--with-arch=${DEFAULT_CPU-armv5te} \
+		    --disable-libgomp"
 	;;
     *)
 	error "Unknown arch"
@@ -240,7 +254,6 @@ build_gcc() {
 	--enable-c99 \
 	--enable-long-long \
 	--enable-cmath \
-	--with-long-double-128 \
 	--disable-multilib \
 	--enable-nls \
 	--disable-werror \
@@ -283,3 +296,9 @@ build_gcc_stage1
 build_uClibc
 
 build_gcc
+
+case $TARGET_ARCH in
+powerpc*|ppc*)
+    . $RULES_DIR/cross-spu.sh
+    ;;
+esac
