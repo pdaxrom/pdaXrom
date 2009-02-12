966e27b4112a8ca71cdd111a7b9915212c8f4968
bubu29 <bubu29@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-02-12 11:13:40 +0000

    - fixed some problems runing this script

--- branches/pdaXrom-ng/build-archos605-crosstools.sh
+++ branches/pdaXrom-ng/build-archos605-crosstools.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-CONF_ARCHOS=1
+export CONF_ARCHOS=1
 
-UCLIBC_CONFIG=archos605-config
-KERNEL_VERSION=2.6.28
+export UCLIBC_CONFIG=archos605-config
+export KERNEL_VERSION=2.6.28
 #KERNEL_CONFIG=archos605_kernel_2.6.10
 #TARGET_VENDOR_PATCH=archos
 
 
 if [ "x$1" = "xclean" ]; then
-   . ./build-crosstools.sh clean
+   ./build-crosstools.sh clean
 else
-   . ./build-crosstools.sh armel-linux-uclibc
+   ./build-crosstools.sh armel-linux-uclibc
 fi
