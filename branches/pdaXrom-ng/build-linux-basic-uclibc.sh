78be4f6ff0d50021b5134b4eb02797048bbd9d2d
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-03-26 10:51:52 +0000

    2.6.29 x86

--- branches/pdaXrom-ng/build-linux-basic-uclibc.sh
+++ branches/pdaXrom-ng/build-linux-basic-uclibc.sh
@@ -4,14 +4,15 @@ TARGET_ARCH="i686-linux-uclibc"
 TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
 TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 
-KERNEL_VERSION="2.6.28"
-KERNEL_CONFIG=i686-kernel-2.6.28
+KERNEL_VERSION="2.6.29"
+KERNEL_CONFIG=i686-kernel-2.6.29
+SQUASHFS_LZMA=no
 
 . ./sets/packages-basic.inc
 
 . $RULES_DIR/tweak-i686.sh
 
-. ./sets/packages-host-squashfs.inc
+. ./sets/packages-host-squashfs4.inc
 . $RULES_DIR/create_squashfs.sh
 . $RULES_DIR/host_syslinux.sh
 . $RULES_DIR/create_x86cd.sh
