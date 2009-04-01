5b563f03b0a1832fa64ba88561ad6b384e66fcba
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-04-01 09:46:34 +0000

    addon icons to gtk theme

---
+++ branches/pdaXrom-ng/rules/lxde-tweaks.sh
@@ -0,0 +1,27 @@
+#
+# packet template
+#
+# Copyright (C) 2009 by Alexander Chukov <sash@pdaXrom.org>
+#          
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+LXDE_TWEAKS_VERSION=0.0
+LXDE_TWEAKS=lxde-tweaks-${LXDE_TWEAKS_VERSION}.zip
+LXDE_TWEAKS_MIRROR=http://no.ya
+LXDE_TWEAKS_DIR=$BUILD_DIR/lxde-tweaks-${LXDE_TWEAKS_VERSION}
+LXDE_TWEAKS_ENV="$CROSS_ENV_AC"
+
+build_lxde_tweaks() {
+    test -e "$STATE_DIR/lxde_tweaks.installed" && return
+    banner "Build lxde-tweaks"
+
+    cp -a $GENERICFS_DIR/themes/gnome $ROOTFS_DIR/usr/share/icons/ || error "install addon lxde icons"
+
+    touch "$STATE_DIR/lxde_tweaks.installed"
+}
+
+build_lxde_tweaks
