5ca48e59bb740fae7ca772f3bf0d907608af711d
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-04-15 12:00:59 +0000

    onscreen keyboard

--- branches/pdaXrom-ng/rules/tweak-ps3.sh
+++ branches/pdaXrom-ng/rules/tweak-ps3.sh
@@ -31,6 +31,16 @@ build_tweak_ps3() {
 	echo "vo=x11" > $ROOTFS_DIR/etc/mplayer/mplayer.conf
     fi
 
+    if [ -e $ROOTFS_DIR/usr/bin/matchbox-keyboard ]; then
+	$INSTALL -D -m 755 $GENERICFS_DIR/mb-keyboard/mb-keyboard-switcher $ROOTFS_DIR/usr/bin/mb-keyboard-switcher || error
+	$INSTALL -D -m 644 $GENERICFS_DIR/mb-keyboard/mb-keyboard-switcher.desktop $ROOTFS_DIR/usr/share/applications/mb-keyboard-switcher.desktop || error
+
+	$INSTALL -D -m 644 $GENERICFS_DIR/mb-keyboard/openbox-rc.xml $ROOTFS_DIR/etc/xdg/openbox/rc.xml || error
+	if [ -d $ROOTFS_DIR/usr/share/lxpanel/profile/default/panels ]; then
+	    $INSTALL -D -m 644 $GENERICFS_DIR/mb-keyboard/lxde-panel $ROOTFS_DIR/usr/share/lxpanel/profile/default/panels/panel || error
+	fi
+    fi
+    
     $INSTALL -D -m 755 $GENERICFS_DIR/etc/init.d/xstart $ROOTFS_DIR/etc/init.d/xstart || error
     install_rc_start xstart 99
     install_rc_stop  xstart 01
