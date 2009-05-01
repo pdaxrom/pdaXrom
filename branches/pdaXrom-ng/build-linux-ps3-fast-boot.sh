c7c5fb305ea3041a2922a5beac83bf80be9a2003
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-05-01 08:41:14 +0000

    ps3 fast boot (one kernel for bootloader and OS) initial

---
+++ branches/pdaXrom-ng/build-linux-ps3-fast-boot.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+TARGET_ARCH="powerpc-linux"
+TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
+TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
+
+KERNEL_VERSION="2.6.29"
+KERNEL_CONFIG=ps3_kernel_2.6.29-fast-boot
+TARGET_VENDOR_PATCH=ps3
+
+SQUASHFS_LZMA=no
+
+. ./sets/packages-basic.inc
+. ./sets/packages-mmlibs.inc
+. ./sets/packages-libs.inc
+
+. $RULES_DIR/wpa_supplicant.sh
+
+. ./sets/packages-xorg-xlib.inc
+. ./sets/packages-x-gtk2.inc
+. ./sets/packages-xorg-xserver.inc
+
+. $RULES_DIR/xf86-input-evdev.sh
+. $RULES_DIR/xf86-input-joystick.sh
+. $RULES_DIR/xf86-input-keyboard.sh
+. $RULES_DIR/xf86-input-mouse.sh
+. $RULES_DIR/xf86-video-fbdev.sh
+
+. ./sets/packages-xorg-apps.inc
+. ./sets/packages-xorg-fonts.inc
+
+. ./sets/packages-xorg-msttcorefonts.inc
+
+#. ./sets/packages-emulators.inc
+
+. ./sets/packages-x-apps.inc
+
+. ./sets/packages-hal.inc
+
+. ./sets/packages-x-lxde.inc
+
+. ./sets/packages-x-office.inc
+
+. ./sets/packages-bluetooth.inc
+
+. ./sets/packages-x-vkeyboard.inc
+
+. $RULES_DIR/sixaxisdmouse.sh
+
+. ./sets/packages-devel.inc
+
+. $RULES_DIR/ps3-utils.sh
+. $RULES_DIR/spufs.sh
+. $RULES_DIR/tweak-ps3.sh
+
+. ./sets/packages-host-squashfs4.inc
+. $RULES_DIR/create_initramfs.sh
+. $RULES_DIR/create_squashfs.sh
+. $RULES_DIR/create_ps3cd.sh
