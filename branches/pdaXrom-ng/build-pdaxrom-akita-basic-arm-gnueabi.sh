67b8a6976a3d94f61ceb723203d0a70f53eadaf7
insearchof <insearchof@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-05-27 00:18:42 +0000

    small changes to allow uboot build.

--- branches/pdaXrom-ng/build-pdaxrom-akita-basic-arm-gnueabi.sh
+++ branches/pdaXrom-ng/build-pdaxrom-akita-basic-arm-gnueabi.sh
@@ -6,7 +6,7 @@ TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 U_BOOT_CONFIG=akita_config
 KERNEL_VERSION="2.6.29.1"
 KERNEL_CONFIG=akita_kernel_2.6.29.1
-TARGET_VENDOR_PATCH=
+TARGET_VENDOR_PATCH=akita
 
 . ./sets/packages-basic.inc
 . $RULES_DIR/u-boot.sh
