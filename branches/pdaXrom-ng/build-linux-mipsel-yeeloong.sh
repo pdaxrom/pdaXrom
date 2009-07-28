2fc1bfae9fd9635b54d09bcd150bdb37e5a9d9c2
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-07-28 11:19:25 +0000

    more yeelong support

---
+++ branches/pdaXrom-ng/build-linux-mipsel-yeeloong.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+TARGET_ARCH="mipsel-ls2f-linux"
+TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
+TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
+
+TARGET_VENDOR_PATCH=ls2f
+
+KERNEL_VERSION="2.6.30.2"
+KERNEL_CONFIG=yeeloong2f_2.6.30.2
+
+. ./sets/packages-basic.inc
+. ./sets/packages-mmlibs.inc
+. ./sets/packages-libs.inc
+
+. ./sets/packages-xorg-xlib.inc
+. ./sets/packages-x-gtk2.inc
+. ./sets/packages-xorg-xserver.inc
+#. ./sets/packages-xorg-drivers.inc
+
+. $RULES_DIR/xf86-input-evdev.sh
+. $RULES_DIR/xf86-input-joystick.sh
+. $RULES_DIR/xf86-input-keyboard.sh
+. $RULES_DIR/xf86-input-mouse.sh
+. $RULES_DIR/xf86-input-synaptics.sh
+. $RULES_DIR/xf86-video-fbdev.sh
+. $RULES_DIR/xf86-video-siliconmotion.sh
+. $RULES_DIR/xf86-video-v4l.sh
+
+. ./sets/packages-xorg-apps.inc
+. ./sets/packages-xorg-fonts.inc
+
+#. ./sets/packages-emulators.inc
+
+. ./sets/packages-x-apps.inc
+
+. ./sets/packages-hal.inc
+
+. ./sets/packages-x-lxde.inc
+
+. ./sets/packages-x-office.inc
+
+#. ./sets/packages-devel.inc
+
+. ./sets/packages-host-squashfs.inc
+. $RULES_DIR/create_squashfs.sh
