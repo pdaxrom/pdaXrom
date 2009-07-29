1f50a6ccb31ab3b64fbe28fc02c1d77f33d6035c
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-07-29 18:39:42 +0000

    arm uclibc gnueabi build script

---
+++ branches/pdaXrom-ng/build-crosstools-arm-uclibcgnueabi.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#export BINUTILS_VERSION=2.19.1
+#export GCC_VERSION=4.4.1
+#export KERNEL_VERSION=2.6.30
+
+#export KERNEL_CONFIG=i686-kernel-2.6.30
+
+#export DEFAULT_CPU="i686"
+#export CROSS_OPT_ARCH="-march=loongson2f"
+#export CROSS_OPT_MABI="-mabi=${DEFAULT_MABI}"
+
+TARGET_ARCH="armel-linux-uclibcgnueabi"
+TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
+
+test -e /opt/${TARGET_ARCH} && mkdir -p /opt/${TARGET_ARCH}
+
+#mkdir -p $TOOLCHAIN_SYSROOT/usr
+#ln -sf lib32 $TOOLCHAIN_SYSROOT/lib
+#ln -sf lib32 $TOOLCHAIN_SYSROOT/usr/lib
+
+if [ "x$1" = "xclean" ]; then
+   ./build-crosstools.sh clean
+else
+   ./build-crosstools.sh $TARGET_ARCH
+fi
