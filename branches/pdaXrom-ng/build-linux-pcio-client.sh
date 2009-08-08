519727f0c14b9c53b06ecabfe82bdd2dcbb40bb4
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-08-08 13:11:27 +0000

    PCiO client build

--- branches/pdaXrom-ng/build-linux-pcio-client.sh
+++ branches/pdaXrom-ng/build-linux-pcio-client.sh
@@ -6,8 +6,9 @@ TARGET_ARCH="i686-linux"
 TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
 TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 
-KERNEL_VERSION="2.6.28"
-KERNEL_CONFIG=i686-kernel-2.6.28
+KERNEL_VERSION="2.6.30"
+KERNEL_CONFIG=i686-kernel-2.6.30
+SQUASHFS_LZMA=no
 
 . ./sets/packages-basic.inc
 . ./sets/packages-mmlibs.inc
@@ -18,16 +19,13 @@ KERNEL_CONFIG=i686-kernel-2.6.28
 . ./sets/packages-xorg-drivers.inc
 . ./sets/packages-xorg-apps.inc
 . ./sets/packages-xorg-fonts.inc
+. ./sets/packages-xorg-msttcorefonts.inc
 
-#. ./sets/packages-simplenx-client.sh
-
-. ./sets/packages-xorg-qt4.inc
-
-. $RULES_DIR/stunnel.sh
+. ./sets/packages-pcio-client.sh
 
 . $RULES_DIR/tweak-i686.sh
 
-. ./sets/packages-host-squashfs.inc
+. ./sets/packages-host-squashfs4.inc
 
 . $RULES_DIR/create_squashfs.sh
 . $RULES_DIR/host_syslinux.sh
