275e238f840c59f8c271005319107f7b4920a1de
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-08-09 06:35:58 +0000

    initramfs lzma compression

--- branches/pdaXrom-ng/rules/create_ps3kboot.sh
+++ branches/pdaXrom-ng/rules/create_ps3kboot.sh
@@ -4,8 +4,8 @@ create_ps3kboot() {
     rm -f $ROOTFS_DIR/linuxrc
     ln -sf /sbin/init $ROOTFS_DIR/init
 
-    ( umask 077; cd $ROOTFS_DIR && find . | cpio -o -H newc | gzip > `get_kernel_ramdisk_path $TARGET_ARCH`; ) || error "create ramdisk"
-    
+    ( umask 077; cd $ROOTFS_DIR && find . | cpio -o -H newc | lzma -9 > `get_kernel_ramdisk_path $TARGET_ARCH`; ) || error "create ramdisk"
+
     local SUBARCH=`get_kernel_subarch $TARGET_ARCH`
 
     pushd $TOP_DIR
