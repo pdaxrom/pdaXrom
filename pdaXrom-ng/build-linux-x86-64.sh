ca58523c4de12a8bcca60d6377be257521a375e1
sashz <sashz@pdaxrom.org>
2009-08-25 12:15:40 +0000

    x86_64 build rules

---
+++ pdaXrom-ng/build-linux-x86-64.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ISOIMAGE_NAME=pdaXrom-ng-x86-64
+
+TARGET_ARCH="x86_64-linux"
+TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
+TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
+
+KERNEL_VERSION="2.6.30"
+KERNEL_CONFIG=x86_64-kernel-2.6.30
+
+. ./sets/packages-basic.inc
+
+. ./sets/packages-acpi.inc
+
+. ./sets/packages-mmlibs.inc
+. ./sets/packages-libs.inc
+
+. $RULES_DIR/wpa_supplicant.sh
+
+. ./sets/packages-xorg-xlib.inc
+. ./sets/packages-x-gtk2.inc
+. ./sets/packages-xorg-xserver.inc
+. ./sets/packages-xorg-drivers.inc
+. ./sets/packages-xorg-apps.inc
+. ./sets/packages-xorg-fonts.inc
+
+. ./sets/packages-xorg-msttcorefonts.inc
+
+#. ./sets/packages-emulators.inc
+
+. ./sets/packages-x-apps.inc
+
+. ./sets/packages-hal.inc
+
+. ./sets/packages-x-lxde.inc
+
+. ./sets/packages-x-office.inc
+
+. ./sets/packages-x-voip.inc
+
+. ./sets/packages-bluetooth.inc
+
+. ./sets/packages-x-vkeyboard.inc
+
+. ./sets/packages-x-rdp.inc
+
+. ./sets/packages-devel.inc
+
+. $RULES_DIR/tweak-i686.sh
+
+. ./sets/packages-host-squashfs.inc
+. $RULES_DIR/create_squashfs.sh
+. $RULES_DIR/host_syslinux.sh
+. $RULES_DIR/create_x86cd.sh
