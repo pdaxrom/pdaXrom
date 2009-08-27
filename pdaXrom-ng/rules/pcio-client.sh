c64e7b6bdcaa4ad8a554f13bebe4b422b1922a8b
sashz <sashz@pdaxrom.org>
2009-08-27 18:02:58 +0000

    mount removables in home directory

--- pdaXrom-ng/rules/pcio-client.sh
+++ pdaXrom-ng/rules/pcio-client.sh
@@ -197,6 +197,8 @@ pcio_tune()
     test -d $ROOTFS_DIR/usr/lib/dri && rm -f $ROOTFS_DIR/usr/lib/dri/*
     $INSTALL -D -m 644 $PCIO_SVN_DIR/desktop/themes/pcio-splash.png $ROOTFS_DIR/usr/share/dancesplashfb/artwork/pcio-splash.png || error
     echo "image /usr/share/dancesplashfb/artwork/pcio-splash.png" > $ROOTFS_DIR/etc/dancesplashfb.conf
+    test -d $ROOTFS_DIR/media && rmdir $ROOTFS_DIR/media
+    ln -sf home $ROOTFS_DIR/media
 }
 
 pcio_tune
