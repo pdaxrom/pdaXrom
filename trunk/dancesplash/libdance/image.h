ac8cd123d6abaa0ae3503a4e32b1d35ba11df253
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-08-27 03:56:38 +0000

    resize image

--- trunk/dancesplash/libdance/image.h
+++ trunk/dancesplash/libdance/image.h
@@ -18,4 +18,6 @@ void db_image_put_image(db_image *dst, db_image *src, int x, int y);
 
 void db_image_put_image_crop(db_image *dst, db_image *src, int x, int y, int xoff, int yoff, int w, int h);
 
+db_image *db_image_resize_image(db_image *src, int width, int height);
+
 #endif
