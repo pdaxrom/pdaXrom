8eacc39192fe8b90447c1fec4719a74110d4839c
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-08-27 04:49:29 +0000

    shm communication

--- trunk/dancesplash/main.c
+++ trunk/dancesplash/main.c
@@ -4,11 +4,18 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
 
 #include "ui.h"
 #include "image.h"
 #include "font.h"
 #include "message.h"
+
+#define SHM_BUF_SIZE	512
+static int shmid;
+static char *shared_buf;
 
 static db_image *img_anim[16];
 
@@ -56,6 +63,27 @@ static db_image *load_wallpaper(db_image *desk, char *name)
     return ret;
 }
 
+static int shared_open(int create)
+{
+    if((shmid = shmget(9999, SHM_BUF_SIZE, (create?IPC_CREAT:0) | 0666)) < 0) {
+        printf("Error in shmget. errno is: %d\n", errno);
+        return -1;
+    }
+    if ((shared_buf = shmat(shmid, NULL, 0)) < 0) {
+	printf("Error in shm attach. errno is: %d\n", errno);
+	return -1;
+    }
+    return 0;
+}
+
+static int shared_close(void)
+{
+    shmdt(shared_buf);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
+
 void app_close(void)
 {
     int i;
@@ -63,11 +91,33 @@ void app_close(void)
 	db_image_free(img_anim[i]);
 
     db_ui_close();
+    shared_close();
 }
 
 int main(int argc, char *argv[])
 {
+    if (argc > 1) {
+	if (!strcmp(argv[1], "-u")) {
+	    if (shared_open(1) == -1) {
+		return -1;
+	    }
+	
+	    strcpy(shared_buf, argv[2]);
+	    shmdt(shared_buf);
+
+	    return 0;
+	}
+    }
+
+    if (shared_open(1) == -1) {
+	return -1;
+    }
+
+#ifdef USE_X11
     db_ui_create_window(480, 480);
+#else
+    db_ui_create();
+#endif
 
     atexit(app_close);
 
@@ -79,12 +129,18 @@ int main(int argc, char *argv[])
 
     
     db_image *img_desk = db_ui_get_screen();
-    db_image *img_wallp = load_wallpaper(img_desk, DATADIR "/artwork/eye.jpg");
+    //db_image *img_wallp = load_wallpaper(img_desk, DATADIR "/artwork/eye.jpg");
+    db_image *img_wallp = db_image_load(DATADIR "/artwork/eye.jpg");
+    if ((img_desk->width != img_wallp->width) ||
+	(img_desk->height != img_wallp->height)) {
+	fprintf(stderr, "resize\n");
+	db_image *tmp = db_image_resize_image(img_wallp, img_desk->width, img_desk->height);
+	db_image_free(img_wallp);
+	img_wallp = tmp;
+    }
     db_image_put_image(img_desk, img_wallp, 0, 0);
 
     load_animation(DATADIR "/artwork/animation/watch%02i.png");
-
-//    db_message_draw(img_desk, font, "PS3 bootshell v0.1\nhttp://wiki.pdaXrom.org", img_desk->width - 50, 50, 400, 120, 0xffffff, DB_WINDOW_COORD_UP_RIGHT);
 
     char *mesg = "Desktop is starting up. Please wait.";
     int w, h;
@@ -99,16 +155,30 @@ int main(int argc, char *argv[])
     while(1) {
 	db_ui_event e;
 	db_ui_check_events(&e);
-	if (e.type == DB_EVENT_QUIT)
+	if (e.type == DB_EVENT_QUIT) {
+	    fprintf(stderr, "DB_EVENT_QUIT\n");
 	    break;
+	}
+#if 0
 	if (e.type == DB_EVENT_SPLASH_PROGRESS) {
 	    if (e.splash.data)
 		free(e.splash.data);
+		fprintf(stderr, "DB_EVENT_SPLASH_PROGRESS\n");
 	    break;
 	}
-	//db_image_put_image(img_desk, img_wallp, 0, 0);
-	//db_font_get_string_box(font, mesg, &w, &h);
-	//db_image_put_string(img_desk, font, mesg, (img_desk->width - w) / 2, img_desk->height - 80, 0xffffff);
+#endif
+	if (*shared_buf) {
+	    fprintf(stderr, ">>> %s\n", shared_buf);
+	    if (!strncmp(shared_buf, "quit", 4))
+		break;
+	    if (!strncmp(shared_buf, "text", 4)) {
+		int w = 0, h = 0;
+		db_image_put_image(img_desk, img_wallp, 0, 0);
+		db_font_get_string_box(font, shared_buf + 5, &w, &h);
+		db_image_put_string(img_desk, font, shared_buf + 5, (img_desk->width - w) / 2, img_desk->height - 80, 0xffffff);
+	    }
+	    *shared_buf = 0;
+	}
 	int n = i % 16;
 	db_image_put_image(img_desk, img_anim[n], (img_desk->width - img_anim[n]->width) / 2, img_desk->height - 40 - img_anim[n]->height / 2);
 	db_ui_update_screen();
