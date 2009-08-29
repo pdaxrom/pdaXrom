f5d8f0fdb320e4226b321d03a2a483c0571609a0
sashz <sashz@pdaxrom.org>
2009-08-29 03:47:09 +0000

    yeeloong kernel 2.6.30.5

--- pdaXrom-ng/build-linux-yeeloong.sh
+++ pdaXrom-ng/build-linux-yeeloong.sh
@@ -8,8 +8,8 @@ TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 
 TARGET_VENDOR_PATCH=ls2f
 
-KERNEL_VERSION="2.6.30.2"
-KERNEL_CONFIG=yeeloong2f_2.6.30.2
+KERNEL_VERSION="2.6.30"
+KERNEL_CONFIG=yeeloong2f_2.6.30.5
 
 . ./sets/packages-basic.inc
 . ./sets/packages-acpi.inc
