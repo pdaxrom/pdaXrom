342dc79bc56b32050464f8f8a4fca717212a1227
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-09-01 05:05:46 +0000

    rpcgen & cpp

--- trunk/pdaXrom-ng/rules/target-dev-tweaks.sh
+++ trunk/pdaXrom-ng/rules/target-dev-tweaks.sh
@@ -1,5 +1,7 @@
 target_dev_tweaks() {
     test -e $ROOTFS_DIR/usr/bin/groups || $INSTALL -D -m 755 $GENERICFS_DIR/groups $ROOTFS_DIR/usr/bin/groups || error "install groups"
+    test -f $TOOLCHAIN_SYSROOT/usr/bin/rpcgen && install_rootfs_usr_bin $TOOLCHAIN_SYSROOT/usr/bin/rpcgen
+    ln -sf ../usr/bin/cpp $ROOTFS_DIR/lib/cpp
     mkdir -p $ROOTFS_DIR/opt
 }
 
