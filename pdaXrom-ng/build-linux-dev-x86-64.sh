337278b8c8244134ecef0846945ca34af0f0b77e
sashz <sashz@pdaxrom.org>
2009-09-08 16:30:43 +0000

    developer images - native gcc & friends

--- pdaXrom-ng/build-linux-dev-x86-64.sh
+++ pdaXrom-ng/build-linux-dev-x86-64.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-ISOIMAGE_NAME=pdaXrom-ng-dev-yeelong-8089-n32
+ISOIMAGE_NAME=pdaXrom-ng-dev-x86-64
 
-TARGET_ARCH="mips64el-ls2f-linux"
+TARGET_ARCH="x86_64-linux"
 TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
 TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 
-TARGET_VENDOR_PATCH=ls2f
-
 KERNEL_VERSION="2.6.30"
-KERNEL_CONFIG=yeeloong2f_2.6.30.5
+KERNEL_CONFIG=x86_64-kernel-2.6.30
 
-CROSS_OPT_ARCH="-march=loongson2f -mtune=loongson2f"
-#CROSS_OPT_CFLAGS="-O3"
-#CROSS_OPT_CXXFLAGS="-O3"
+USE_SPLASH="yes"
 
 . ./sets/packages-basic.inc
+
 . ./sets/packages-acpi.inc
+
 . ./sets/packages-mmlibs.inc
 . ./sets/packages-libs.inc
 
@@ -25,17 +23,7 @@ CROSS_OPT_ARCH="-march=loongson2f -mtune=loongson2f"
 . ./sets/packages-xorg-xlib.inc
 . ./sets/packages-x-gtk2.inc
 . ./sets/packages-xorg-xserver.inc
-#. ./sets/packages-xorg-drivers.inc
-
-. $RULES_DIR/xf86-input-evdev.sh
-. $RULES_DIR/xf86-input-joystick.sh
-. $RULES_DIR/xf86-input-keyboard.sh
-. $RULES_DIR/xf86-input-mouse.sh
-. $RULES_DIR/xf86-input-synaptics.sh
-. $RULES_DIR/xf86-video-fbdev.sh
-. $RULES_DIR/xf86-video-siliconmotion.sh
-. $RULES_DIR/xf86-video-v4l.sh
-
+. ./sets/packages-xorg-drivers.inc
 . ./sets/packages-xorg-apps.inc
 . ./sets/packages-xorg-fonts.inc
 
@@ -53,14 +41,18 @@ CROSS_OPT_ARCH="-march=loongson2f -mtune=loongson2f"
 
 . ./sets/packages-x-voip.inc
 
+. ./sets/packages-bluetooth.inc
+
+. ./sets/packages-x-vkeyboard.inc
+
+. ./sets/packages-x-rdp.inc
+
 . ./sets/packages-mc.inc
 . ./sets/packages-devel.inc
 
-. $RULES_DIR/fnkey-yeeloong2f.sh
-. $RULES_DIR/tweak-yeelong2f.sh
+. $RULES_DIR/tweak-i686.sh
 
 . ./sets/packages-host-squashfs.inc
-. $RULES_DIR/create_initramfs.sh
 . $RULES_DIR/create_squashfs.sh
-
-. $RULES_DIR/create_lemote.sh
+. $RULES_DIR/host_syslinux.sh
+. $RULES_DIR/create_x86cd.sh
