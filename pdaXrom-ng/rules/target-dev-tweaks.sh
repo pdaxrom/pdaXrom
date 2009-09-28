a86a9ab7c33c3bc3abc00a7b2f2fb877a834f90f
sashz <sashz@pdaxrom.org>
2009-09-28 16:16:24 +0000

    ldd

--- pdaXrom-ng/rules/target-dev-tweaks.sh
+++ pdaXrom-ng/rules/target-dev-tweaks.sh
@@ -1,6 +1,7 @@
 target_dev_tweaks() {
     test -e $ROOTFS_DIR/usr/bin/groups || $INSTALL -D -m 755 $GENERICFS_DIR/groups $ROOTFS_DIR/usr/bin/groups || error "install groups"
     test -f $TOOLCHAIN_SYSROOT/usr/bin/rpcgen && install_rootfs_usr_bin $TOOLCHAIN_SYSROOT/usr/bin/rpcgen
+    test -f $TOOLCHAIN_SYSROOT/usr/bin/ldd && $INSTALL -D -m 755 $TOOLCHAIN_SYSROOT/usr/bin/ldd $ROOTFS_DIR/usr/bin/ldd
     test -f $TOOLCHAIN_SYSROOT/sbin/ldconfig && $INSTALL -D -m 755 $TOOLCHAIN_SYSROOT/sbin/ldconfig $ROOTFS_DIR/sbin/ldconfig && $STRIP $ROOTFS_DIR/sbin/ldconfig
     ln -sf ../usr/bin/cpp $ROOTFS_DIR/lib/cpp
     mkdir -p $ROOTFS_DIR/opt
