1cb9f4a5f17d0c3a95a1aabab11c4aeb0e28dc4b
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-10-13 09:03:02 +0000

    uboot build rules

--- trunk/pdaXrom-ng/rules/u-boot.sh
+++ trunk/pdaXrom-ng/rules/u-boot.sh
@@ -2,19 +2,23 @@
 # packet template
 #
 # Copyright (C) 2009 by Adrian Crutchfield <insearchof@pdaxrom.org>
-#          
+#
 # See CREDITS for details about who has contributed to this project.
 #
 # For further information about the pdaXrom project and license conditions
 # see the README file.
 #
 
-U_BOOT_VERSION=2006-04-18-1106
+U_BOOT_VERSION=${U_BOOT_VERSION-2009.08}
 U_BOOT=u-boot-${U_BOOT_VERSION}.tar.bz2
-U_BOOT_MIRROR=http://mail.pdaxrom.org/src/
+U_BOOT_MIRROR=ftp://ftp.denx.de/pub/u-boot
 U_BOOT_DIR=$BUILD_DIR/u-boot-${U_BOOT_VERSION}
 U_BOOT_ENV="$CROSS_ENV_AC"
 CROSS_COMPILE=${TARGET_ARCH}-
+
+if [ "x$U_BOOT_CONFIG" = "x" ]; then
+    error "No U-BOOT config defined!"
+fi
 
 build_u_boot() {
     test -e "$STATE_DIR/u_boot.installed" && return
@@ -24,16 +28,14 @@ build_u_boot() {
     apply_patches $U_BOOT_DIR $U_BOOT
     pushd $TOP_DIR
     cd $U_BOOT_DIR
-    (
-    eval \
-	$CROSS_CONF_ENV \
-	$U_BOOT_ENV 
-   ) 
-    make $U_BOOT_CONFIG
-    make -C $U_BOOT_DIR CROSS_COMPILE=${TARGET_ARCH}- 
- 
+
+    make ${U_BOOT_CONFIG}_config || error "configure uboot"
+    make CROSS_COMPILE=${TARGET_ARCH}- || error "uboot build"
+
+    cp u-boot.bin $ROOTFS_DIR $IMAGES_DIR/
+
     popd
-    cp $U_BOOT_DIR/u-boot.bin $ROOTFS_DIR $IMAGES_DIR/
+
     touch "$STATE_DIR/u_boot.installed"
 }
 
