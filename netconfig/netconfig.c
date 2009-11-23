965a612a1dd2fa1d13e4c81d95aab0b6c3d5873f
sashz <sashz@pdaxrom.org>
2009-11-23 08:28:27 +0000

    resource directory, license

--- netconfig/netconfig.c
+++ netconfig/netconfig.c
@@ -1,8 +1,31 @@
+/* 
+ * Copyright (C) 2009 Alexander Chukov <sash@pdaXrom.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <malloc.h>
+
+#ifndef RESDIR
+#define RESDIR "."
+#endif
 
 #define IFACE_CONFIG	"/etc/network/interfaces"
 //#define IFACE_CONFIG	"interfaces"
@@ -335,7 +358,7 @@ int main (int argc, char **argv)
 
     /* Create the main window */
     gtkBuilder= gtk_builder_new();
-    gtk_builder_add_from_file(gtkBuilder,"netconfig.ui",NULL);
+    gtk_builder_add_from_file(gtkBuilder,RESDIR "/netconfig.ui",NULL);
 
     mainwin= GTK_WIDGET(gtk_builder_get_object(gtkBuilder,"window1"));
 
