57f72ec9b6cca4e8d842b39ff21f0dc5b5f30e86
insearchof <insearchof@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2009-12-05 19:50:30 +0000

    Added if statement to uboot.sh for old svn version (Only Temporary) Added kernel patches for zaurus (akita/spitz/borzoi) Zaurus Akita/Borzoi Emergency Boot working

--- trunk/pdaXrom-ng/platforms/zaurus/build-pdaxrom-akita-basic-arm-gnueabi.sh
+++ trunk/pdaXrom-ng/platforms/zaurus/build-pdaxrom-akita-basic-arm-gnueabi.sh
@@ -3,10 +3,14 @@
 TARGET_ARCH="armel-linux-gnueabi"
 TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
 TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
-U_BOOT_CONFIG=akita_config
-KERNEL_VERSION="2.6.29.1"
-KERNEL_CONFIG=akita_kernel_2.6.29.1
-TARGET_VENDOR_PATCH=akita
+
+TARGET_VENDOR_PATCH="zaurus"
+
+KERNEL_VERSION="2.6.32"
+KERNEL_CONFIG=akita_kernel_2.6.32
+
+U_BOOT_CONFIG=akita
+U_BOOT_VERSION=2006-04-18-1106
 
 . $SETS_DIR/packages-basic.inc
 . $RULES_DIR/u-boot.sh
