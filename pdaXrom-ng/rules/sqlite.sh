f6eb6c7f69a56aa44e72ed3d2b13ecaf9506302a
sashz <sashz@pdaxrom.org>
2010-01-11 17:55:11 +0000

    ff update

--- pdaXrom-ng/rules/sqlite.sh
+++ pdaXrom-ng/rules/sqlite.sh
@@ -1,17 +1,18 @@
 #
 # packet template
 #
-# Copyright (C) 2008 by Alexander Chukov <sash@pdaXrom.org>
-#          
+# Copyright (C) 2009 by Alexander Chukov <sash@pdaXrom.org>
+#
 # See CREDITS for details about who has contributed to this project.
 #
 # For further information about the pdaXrom project and license conditions
 # see the README file.
 #
 
-SQLITE=sqlite-3.6.7.tar.gz
-SQLITE_MIRROR=http://www.sqlite.org
-SQLITE_DIR=$BUILD_DIR/sqlite-3.6.7
+SQLITE_VERSION=3.6.22
+SQLITE=sqlite-${SQLITE_VERSION}.tar.gz
+SQLITE_MIRROR=http://sqlite.org
+SQLITE_DIR=$BUILD_DIR/sqlite-${SQLITE_VERSION}
 SQLITE_ENV="$CROSS_ENV_AC"
 
 build_sqlite() {
@@ -29,18 +30,15 @@ build_sqlite() {
 	./configure --build=$BUILD_ARCH --host=$TARGET_ARCH \
 	    --prefix=/usr \
 	    --sysconfdir=/etc \
-	    -disable-tcl \
 	    || error
     ) || error "configure"
-    
+
     make $MAKEARGS || error
 
     install_sysroot_files || error
-    
-    $INSTALL -D -m 644 .libs/libsqlite3.so.0.8.6 $ROOTFS_DIR/usr/lib/libsqlite3.so.0.8.6 || error
-    ln -sf libsqlite3.so.0.8.6 $ROOTFS_DIR/usr/lib/libsqlite3.so.0
-    ln -sf libsqlite3.so.0.8.6 $ROOTFS_DIR/usr/lib/libsqlite3.so
-    $STRIP $ROOTFS_DIR/usr/lib/libsqlite3.so.0.8.6
+
+    install_fakeroot_init
+    install_fakeroot_finish || error
 
     popd
     touch "$STATE_DIR/sqlite.installed"
