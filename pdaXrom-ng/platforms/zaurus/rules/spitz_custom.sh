d1a81c592fae0a096013aa4dc8901a3d44c783b4
insearchof <insearchof@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-02-12 00:52:21 +0000

    Updated packages for Spitz Full Build Added calibration to zaurushw and moved zaurushw from S99 to S98 to run before SLiM Fixed variable for enabling the tslib xorg module in packages-xorg-drivers

--- pdaXrom-ng/platforms/zaurus/rules/spitz_custom.sh
+++ pdaXrom-ng/platforms/zaurus/rules/spitz_custom.sh
@@ -1,7 +1,7 @@
 create_customizations() {
     
     $INSTALL -m 755 $BSP_GENERICFS_DIR/etc/init.d/zaurushw $ROOTFS_DIR/etc/init.d/ || error
-    install_rc_start zaurushw 99
+    install_rc_start zaurushw 98
 
     for f in $BSP_GENERICFS_DIR/etc/*; do
 	test -f $f && $INSTALL -m 644 $f $ROOTFS_DIR/etc/
