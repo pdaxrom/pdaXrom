082b52925b74ea1f7a619fb971a5f6e70885a557
insearchof <insearchof@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-02-12 00:52:21 +0000

    Updated packages for Spitz Full Build Added calibration to zaurushw and moved zaurushw from S99 to S98 to run before SLiM Fixed variable for enabling the tslib xorg module in packages-xorg-drivers

--- trunk/pdaXrom-ng/platforms/zaurus/build-spitz-full.sh
+++ trunk/pdaXrom-ng/platforms/zaurus/build-spitz-full.sh
@@ -34,17 +34,122 @@ USE_LOGINMANAGER="yes"
 . $SETS_DIR/packages-xorg-apps.inc
 . $SETS_DIR/packages-xorg-fonts.inc
 
-#. $SETS_DIR/packages-emulators.inc
-
-. $SETS_DIR/packages-x-apps.inc
 . $RULES_DIR/openbox.sh
 . $RULES_DIR/xmodmap.sh
-
-#. $SETS_DIR/packages-hal.inc
-
 . $SETS_DIR/packages-x-lxde.inc
 
+. $SETS_DIR/packages-gconf.inc
+
+#
+# Firefox 3.5
+#
+. $RULES_DIR/sqlite.sh
+. $RULES_DIR/libogg.sh
+. $RULES_DIR/libvorbis.sh
+. $RULES_DIR/host_findutils.sh
+. $RULES_DIR/host_glib2.sh
+. $RULES_DIR/host_libIDL.sh
+. $RULES_DIR/libIDL.sh
+. $RULES_DIR/firefox-3.5.sh
+
+. $RULES_DIR/target-bash.sh
+. $RULES_DIR/slim.sh
+
+# Minimal X display lock program
+. $RULES_DIR/xtrlock.sh
+
+#
+# xchat
+#
+. $RULES_DIR/libsexy.sh
+. $RULES_DIR/xchat.sh
+
+#
+# transmission
+#
+. $RULES_DIR/curl.sh
+. $RULES_DIR/transmission.sh
+
+#
+# mplayer
+#
+. $RULES_DIR/SDL.sh
+. $RULES_DIR/libogg.sh
+. $RULES_DIR/libvorbis.sh
+. $RULES_DIR/speex.sh
+#. $RULES_DIR/libtheora.sh
+. $RULES_DIR/libv4l.sh
+. $RULES_DIR/mplayer.sh
+. $RULES_DIR/clearplayer.sh
+
+#
+# vlc
+#
+if [ "$USE_VLC_PLAYER" = "yes" ]; then
+. $RULES_DIR/SDL.sh
+. $RULES_DIR/libogg.sh
+. $RULES_DIR/libvorbis.sh
+. $RULES_DIR/speex.sh
+#. $RULES_DIR/libtheora.sh
+. $RULES_DIR/libv4l.sh
+. $RULES_DIR/libmad.sh
+. $RULES_DIR/SDL_image.sh
+. $RULES_DIR/a52dec.sh
+. $RULES_DIR/libmpeg2.sh
+. $RULES_DIR/libmtp.sh
+#. $RULES_DIR/live555.sh
+. $RULES_DIR/zvbi.sh
+. $RULES_DIR/libdvbpsi5.sh
+. $RULES_DIR/libcddb.sh
+. $RULES_DIR/libcdio.sh
+. $RULES_DIR/popt.sh
+. $RULES_DIR/vcdimager.sh
+. $RULES_DIR/libupnp.sh
+. $RULES_DIR/host_yasm.sh
+. $RULES_DIR/x264-snapshot.sh
+. $RULES_DIR/libdvdcss.sh
+. $RULES_DIR/libdvdread.sh
+. $RULES_DIR/libdvdnav.sh
+. $RULES_DIR/taglib.sh
+. $RULES_DIR/libproxy.sh
+. $RULES_DIR/flac.sh
+. $RULES_DIR/readline.sh
+. $RULES_DIR/lua.sh
+. $RULES_DIR/libfribidi.sh
+. $RULES_DIR/qt-all-opensource-src.sh
+. $RULES_DIR/vlc.sh
+
+#
+# Arora
+#
+#. $RULES_DIR/arora.sh
+fi
+
+
+#
+# Texteditor
+#
+. $RULES_DIR/leafpad.sh
+
+#
+# Image viewer
+#
+. $RULES_DIR/gpicview.sh
+
+#
+# Archiver
+#
+. $RULES_DIR/unzip.sh
+. $RULES_DIR/zip.sh
+. $RULES_DIR/unrar.sh
+. $RULES_DIR/xarchiver.sh
+
+
+
 #. $SETS_DIR/packages-x-office.inc
+#. $SETS_DIR/packages-emulators.inc
+#. $SETS_DIR/packages-hal.inc
+#. $SETS_DIR/packages-x-apps.inc
 
 . $RULES_DIR/imlib2.sh
 . $RULES_DIR/giblib.sh
