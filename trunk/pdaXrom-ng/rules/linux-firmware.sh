b89f61fb534c806833b1f17a12e5ff2e3b589714
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-03-25 18:38:46 +0000

    ralink firmware

---
+++ trunk/pdaXrom-ng/rules/linux-firmware.sh
@@ -0,0 +1,31 @@
+#
+# packet template
+#
+# Copyright (C) 2009 by Alexander Chukov <sash@pdaXrom.org>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the pdaXrom project and license conditions
+# see the README file.
+#
+
+LINUX_FIRMWARE_VERSION=1.24
+LINUX_FIRMWARE=linux-firmware_${LINUX_FIRMWARE_VERSION}.tar.gz
+LINUX_FIRMWARE_MIRROR=http://archive.ubuntu.com/ubuntu/pool/main/l/linux-firmware
+LINUX_FIRMWARE_DIR=$BUILD_DIR/linux-firmware-${LINUX_FIRMWARE_VERSION}
+LINUX_FIRMWARE_ENV="$CROSS_ENV_AC"
+
+build_linux_firmware() {
+    test -e "$STATE_DIR/linux_firmware.installed" && return
+    banner "Build linux-firmware"
+    download $LINUX_FIRMWARE_MIRROR $LINUX_FIRMWARE
+    extract $LINUX_FIRMWARE
+    apply_patches $LINUX_FIRMWARE_DIR $LINUX_FIRMWARE
+    pushd $TOP_DIR
+    cd $LINUX_FIRMWARE_DIR
+
+    popd
+    touch "$STATE_DIR/linux_firmware.installed"
+}
+
+build_linux_firmware
