1587ba5c1afe2c884579e7df2f120074dde08ead
sashz <sashz@pdaxrom.org>
2010-05-18 13:31:37 +0000

    stable version, 18 may 2010

--- dancesplash/libdance/readconf.h
+++
@@ -1,6 +0,0 @@
-#ifndef _READCONF_H_
-#define _READCONF_H_
-
-int db_readconf(char *fname, char *name, char *out);
-
-#endif
--- ps3boot/pngdec.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PNGDEC_H_
-#define _PNGDEC_H_
-
-unsigned char *load_png_file (char *file, int *width, int *height, int *has_alphar);
-
-#endif
