9ba81429465613973f0bb529acfafbdba509bb9b
insearchof <insearchof@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-05-28 14:55:44 +0000

    Added Spitz 2.6.34 support and patches Change barebox to look for defconfig not config

--- pdaXrom-ng/platforms/zaurus/build-spitz-basic.sh
+++ pdaXrom-ng/platforms/zaurus/build-spitz-basic.sh
@@ -9,7 +9,7 @@ TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 
 TARGET_VENDOR_PATCH="zaurus"
 
-KERNEL_VERSION="2.6.32"
+KERNEL_VERSION="2.6.34"
 KERNEL_CONFIG=akita_kernel_2.6.32
 
 . $SETS_DIR/packages-basic.inc
