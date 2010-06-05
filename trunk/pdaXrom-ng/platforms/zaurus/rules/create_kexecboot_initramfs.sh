6b6c65a4ec59c6d9d4072fedb39d24998831d016
insearchof <insearchof@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-06-05 00:09:41 +0000

    Added devices to create_kexecboot_initramfs.sh Removed CONFIG_LOGO from kernel config Added kexec-tools-2.0.1

--- trunk/pdaXrom-ng/platforms/zaurus/rules/create_kexecboot_initramfs.sh
+++ trunk/pdaXrom-ng/platforms/zaurus/rules/create_kexecboot_initramfs.sh
@@ -9,9 +9,9 @@ create_initramfs() {
     done	
 
 #    $INSTALL -D -m 755 $BSP_GENERICFS_DIR/scripts/init_kexecboot $INITRAMFS_DIR/init || error
-#    $INSTALL -D -m 755 $ROOTFS_DIR/usr/bin/loadkeys $INITRAMFS_DIR/bin/loadkeys || error
+    $INSTALL -D -m 755 $ROOTFS_DIR/usr/sbin/kexec $INITRAMFS_DIR/usr/sbin/kexec || error
     $INSTALL -D -m 755 $ROOTFS_DIR/usr/bin/kexecboot $INITRAMFS_DIR/init || error
-
+    
     uuidgen > $INITRAMFS_DIR/uuid
     cp -f $INITRAMFS_DIR/uuid $IMAGES_DIR/uuid
 
@@ -27,6 +27,21 @@ create_initramfs() {
     echo "nod /dev/tty2    0640 0 0 c 4 2" >> $BUILD_DIR/initramfs.list
     echo "nod /dev/tty3    0640 0 0 c 4 3" >> $BUILD_DIR/initramfs.list
     echo "nod /dev/null    0666 0 0 c 1 3" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/hda     0660	0 0 b 3 0" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/hda     0660 0 0 b 3 1" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/mtd     0660 0 0 c 90 0" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/mtdblock 0660 0 0 b 31 0" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/event   0660 0 0 c 13 64" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/input/event   0660 0 0 c 13 64" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/mmcblk0 0660 0 0 b 179 0" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/mmcblk0p 0660 0 0 b 179 1" >> $BUILD_DIR/initramfs.list
+#    echo "nod /dev/initctl 0600 0 0 p" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/kmem   0640 0 0 c 1 2" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/mem   0660 0 0 c 1 1" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/ram   0640 0 0 b 1 0" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/random 0644 0 0 c 1 8" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/urandom 0644 0 0 c 1 9" >> $BUILD_DIR/initramfs.list
+    echo "nod /dev/ptmx   0644 0 0 c 5 2" >> $BUILD_DIR/initramfs.list
 
     make SUBARCH=$SUBARCH CROSS_COMPILE=${TOOLCHAIN_PREFIX}/bin/${CROSS} $MAKEARGS `get_kernel_image $TARGET_ARCH` CONFIG_INITRAMFS_SOURCE=$BUILD_DIR/initramfs.list || error
 
