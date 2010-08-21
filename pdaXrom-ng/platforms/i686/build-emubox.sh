34fd00b07e93cd17013a5aff86f13d762d1057f6
sashz <sashz@pdaxrom.org>
2010-08-21 12:04:45 +0000

    EmuBOX

--- pdaXrom-ng/platforms/i686/build-emubox.sh
+++ pdaXrom-ng/platforms/i686/build-emubox.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-ISOIMAGE_NAME="ps3-emubox"
+ISOIMAGE_NAME="i686-emubox"
+FAT_TITLE="i686 EmuBOX"
+FATIMAGE_NAME="i686-emubox"
 
-TARGET_ARCH="powerpc-ps3-linux"
+BOOTFAT_IMAGE_SIZE=250
+
+TARGET_ARCH="i686-linux"
 TOOLCHAIN_PREFIX="/opt/${TARGET_ARCH}/toolchain"
 TOOLCHAIN_SYSROOT="/opt/${TARGET_ARCH}/sysroot"
 
 KERNEL_VERSION="2.6.32"
-KERNEL_CONFIG=ps3_kernel_2.6.32
-TARGET_VENDOR_PATCH=ps3
-
-LIBC_GCONV_MODULES="CP1252.so"
+KERNEL_CONFIG=i686-kernel-2.6.32
 
 . ./rules/core.sh
 
@@ -69,19 +70,18 @@ LIBC_GCONV_MODULES="CP1252.so"
 #. $RULES_DIR/gdb.sh
 #. $RULES_DIR/strace.sh
 
-. $SETS_DIR/packages-bluez3.inc
-. $RULES_DIR/sixaxisdmouse.sh
+#. $SETS_DIR/packages-bluez3.inc
+#. $RULES_DIR/sixaxisdmouse.sh
 
 . $RULES_DIR/minimount-svn.sh
+. $RULES_DIR/915resolution.sh
+. $RULES_DIR/v86d.sh
 
-. $RULES_DIR/ps3-utils.sh
-. $RULES_DIR/spufs.sh
-. $RULES_DIR/tweak-ps3.sh
+. $RULES_DIR/tweak-i686.sh
 
 . $SETS_DIR/packages-host-squashfs.inc
 . $RULES_DIR/create_squashfs.sh
-
-KBOOT_CONFIG=${BSP_GENERICFS_DIR}/emubox/kboot.conf
-KBOOT_MESSAGE=${BSP_GENERICFS_DIR}/emubox/kboot.msg
-
-. $RULES_DIR/create_ps3cd.sh
+. $RULES_DIR/host_syslinux.sh
+. $RULES_DIR/create_x86cd.sh
+. $RULES_DIR/host_makebootfat.sh
+. $RULES_DIR/create_bootfat.sh
