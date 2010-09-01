dd931f46b15d5283e1e2824d6bdcb56d7dda7878
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-09-01 07:07:16 +0000

    cups update

--- trunk/pdaXrom-ng/rules/cups.sh
+++ trunk/pdaXrom-ng/rules/cups.sh
@@ -9,7 +9,7 @@
 # see the README file.
 #
 
-CUPS_VERSION=1.4rc1
+CUPS_VERSION=1.4.4
 #CUPS_VERSION=1.3.11
 CUPS=cups-${CUPS_VERSION}-source.tar.bz2
 CUPS_MIRROR=http://ftp.easysw.com/pub/cups/${CUPS_VERSION}
@@ -34,17 +34,16 @@ build_cups() {
 	    --localstatedir=/var \
 	    --disable-gssapi \
 	    --disable-ldap \
+	    --with-openssl-libs=${TARGET_LIB} \
+	    --with-openssl-includes=${TARGET_INC} \
 	    || error
     ) || error "configure"
     
     make $MAKEARGS GENSTRINGS=true || error
 
-    rm -rf fakeroot
+    install_sysroot_files GENSTRINGS=true BUILDROOT=${TARGET_BIN_DIR} || error
 
-    make $MAKEARGS GENSTRINGS=true BUILDROOT=${CUPS_DIR}/fakeroot install || error
-
-    cp -a fakeroot/usr/include/cups $TARGET_INC/ || error
-    cp -a fakeroot/usr/lib/*.* $TARGET_LIB/ || error
+    install_fakeroot_init  GENSTRINGS=true BUILDROOT=${CUPS_DIR}/fakeroot
 
     cp -a fakeroot/usr/bin/cups-config $HOST_BIN_DIR/bin/ || error
 
@@ -53,19 +52,10 @@ build_cups() {
 	    -e "/^libdir=/s:\(libdir=\)\(/lib\|/usr/lib\):\1${TARGET_BIN_DIR}\2:g"	\
 	    $HOST_BIN_DIR/bin/cups-config || true
 
-    rm -rf fakeroot/usr/include fakeroot/usr/share/doc fakeroot/usr/share/locale
-
     rm -rf fakeroot/etc/rc[0-9].d fakeroot/etc/init.d
-
-    find fakeroot/usr/ ! -type d -a ! -type l | xargs $STRIP
-
-    mv -f fakeroot/usr/share/icons/hicolor fakeroot/usr/share/icons/gnome || error
-
     rm -f $ROOTFS_DIR/usr/bin/lpr $ROOTFS_DIR/usr/bin/lpq
 
-    cp -a fakeroot/etc $ROOTFS_DIR/ || error
-    cp -a fakeroot/usr $ROOTFS_DIR/ || error
-    cp -a fakeroot/var $ROOTFS_DIR/ || error
+    install_fakeroot_finish || error
 
     $INSTALL -D -m 755 $GENERICFS_DIR/etc/init.d/cups $ROOTFS_DIR/etc/init.d/cups || error
     install_rc_start cups 85
