320911e031faa75797d77675ccf6197da4a4644a
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-12-06 14:00:54 +0000

    squashfs tools 4

--- trunk/pdaXrom-ng/rules/host_squashfs4.sh
+++ trunk/pdaXrom-ng/rules/host_squashfs4.sh
@@ -2,17 +2,17 @@
 # host packet template
 #
 # Copyright (C) 2009 by Alexander Chukov <sash@pdaXrom.org>
-#          
+#
 # See CREDITS for details about who has contributed to this project.
 #
 # For further information about the pdaXrom project and license conditions
 # see the README file.
 #
 
-HOST_SQUASHFS4_VERSION=20090125
-HOST_SQUASHFS4=squashfs-${HOST_SQUASHFS4_VERSION}.tar.bz2
-HOST_SQUASHFS4_MIRROR=http://mail.pdaXrom.org/downloads/src
-HOST_SQUASHFS4_DIR=$HOST_BUILD_DIR/squashfs-${HOST_SQUASHFS4_VERSION}
+HOST_SQUASHFS4_VERSION=4.1
+HOST_SQUASHFS4=squashfs${HOST_SQUASHFS4_VERSION}.tar.gz
+HOST_SQUASHFS4_MIRROR=http://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.1
+HOST_SQUASHFS4_DIR=$HOST_BUILD_DIR/squashfs${HOST_SQUASHFS4_VERSION}
 HOST_SQUASHFS4_ENV=
 
 build_host_squashfs4() {
@@ -22,11 +22,10 @@ build_host_squashfs4() {
     extract_host $HOST_SQUASHFS4
     apply_patches $HOST_SQUASHFS4_DIR $HOST_SQUASHFS4
     pushd $TOP_DIR
-    cd $HOST_SQUASHFS4_DIR
-
-    make $MAKEARGS -C squashfs-tools || error
-
-    $INSTALL -D -m 755 squashfs-tools/mksquashfs $HOST_BIN_DIR/bin/mksquashfs || error
+    cd $HOST_SQUASHFS4_DIR/squashfs-tools
+    sed -i -e 's|XATTR_SUPPORT = 1|# XATTR_SUPPORT = 1|' Makefile
+    make $MAKEARGS XZ_SUPPORT=1 INCLUDEDIR=-I${HOST_BIN_DIR}/include LIBS="-Wl,-rpath,${HOST_BIN_DIR}/lib -L${HOST_BIN_DIR}/lib -llzma -lz" || error
+    make $MAKEARGS install INSTALL_DIR=${HOST_BIN_DIR}/bin || error
 
     popd
     touch "$STATE_DIR/host_squashfs4.installed"
