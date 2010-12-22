32571ac248140f6d6932911f486ee57adfd6fef1
sashz <sashz@1b8ddeed-2415-0410-b2c2-c8836e3aacde>
2010-12-22 20:00:09 +0000

    mini xserver config, fb only

--- trunk/pdaXrom-ng/rules/xorg-server-kdrive.sh
+++ trunk/pdaXrom-ng/rules/xorg-server-kdrive.sh
@@ -9,7 +9,7 @@
 # see the README file.
 #
 
-XORG_SERVER_VERSION=1.6.3
+XORG_SERVER_VERSION=1.9.2
 XORG_SERVER=xorg-server-${XORG_SERVER_VERSION}.tar.bz2
 XORG_SERVER_MIRROR=ftp://ftp.freedesktop.org/pub/xorg/individual/xserver
 XORG_SERVER_DIR=$BUILD_DIR/xorg-server-${XORG_SERVER_VERSION}
@@ -19,8 +19,8 @@ XORG_SERVER_ENV=
 #	ac_cv_asm_mtrr_h=no \
 #	ac_cv_header_linux_agpgart_h=no"
 
-build_xorg_server() {
-    test -e "$STATE_DIR/xorg_server-${XORG_SERVER_VERSION}" && return
+build_xorg_server_kdrive() {
+    test -e "$STATE_DIR/xorg_server_kdrive-${XORG_SERVER_VERSION}" && return
     banner "Build $XORG_SERVER"
     download $XORG_SERVER_MIRROR $XORG_SERVER
     extract $XORG_SERVER
@@ -41,18 +41,22 @@ build_xorg_server() {
 	    --sysconfdir=/etc \
 	    --localstatedir=/var \
 	    --datadir=/usr/share \
+	    --enable-shm \
+	    --enable-freetype \
 	    --disable-dri \
 	    --disable-glx \
-	    --enable-xorg=no \
-	    --enable-xvfb=no \
-	    --enable-xnest=no \
-	    --enable-xephyr=no \
-	    --enable-xfake=no \
+	    --disable-xorg \
+	    --disable-dmx \
+	    --disable-xvfb \
+	    --disable-xnest \
+	    --disable-kdrive \
+	    --disable-xephyr \
+	    --disable-xfake \
 	    --enable-kdrive \
 	    --enable-xfbdev \
-	    $XORG_SERVER_TSLIB_CONF \
 	    --with-fontdir=/usr/share/fonts \
 	    --without-dtrace \
+	    $XORG_SERVER_TSLIB_CONF \
 	    --with-xkb-output=/var/lib/xkb/compiled \
 	    || error
     ) || error
@@ -60,19 +64,20 @@ build_xorg_server() {
     make $MAKEARGS || error
 
     install_sysroot_files || error
-    
-    make DESTDIR=$XORG_SERVER_DIR/fakeroot install || error
-    
-    cd $XORG_SERVER_DIR/fakeroot
-    
-    mkdir -p $ROOTFS_DIR/var/lib/xkb/compiled || error
 
-    $INSTALL -D -m 755 ./usr/bin/Xfbdev $ROOTFS_DIR/usr/bin/Xfbdev || error
-    ln -sf Xfbdev $ROOTFS_DIR/usr/bin/X
-    $STRIP $ROOTFS_DIR/usr/bin/Xfbdev
+    install_fakeroot_init
+    #ln -sf Xfbdev $ROOTFS_DIR/usr/bin/X
+    install_fakeroot_finish || error
+
+#
+#
+#    cp -f hw/xfree86/parser/xf86Optrec.h ${TARGET_INC}/xorg/ || error "workaround 1.6.2"
+#    cp -f hw/xfree86/parser/xf86Parser.h ${TARGET_INC}/xorg/ || error "workaround 1.6.2"
+#
+#
 
     popd
-    touch "$STATE_DIR/xorg_server-${XORG_SERVER_VERSION}"
+    touch "$STATE_DIR/xorg_server_kdrive-${XORG_SERVER_VERSION}"
 }
 
-build_xorg_server
+build_xorg_server_kdrive
